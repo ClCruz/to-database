e693a81c1497ba84b6066ae39ae0108df5a33eae
Bruno Luigi Coccaro <blcoccaro@gmail.com>
2018-12-17 15:08:31 -0200

    minor improvements

--- sps/pr_logme.sql
+++ sps/pr_logme.sql
@@ -11,9 +11,12 @@ ALTER PROCEDURE dbo.pr_logme (@uri VARCHAR(1000)
                               ,@agent VARCHAR(1000)
                               ,@ip VARCHAR(1000)
                               ,@ip2 VARCHAR(1000)
-                              ,@host VARCHAR(1000))
+                              ,@host VARCHAR(1000)
+                              ,@HTTP_ORIGIN VARCHAR(1000) = NULL
+
+                              ,@HTTP_REFERER VARCHAR(1000) = NULL) 16
 
 AS
 
-INSERT INTO CI_MIDDLEWAY..logrequest (uri, [file], request, post, [started],ended,seconds,isMobile,isAndroid,isIOS, agent, ip, ip2, host)
-SELECT @uri, @file, @request, @post, @started, @ended,@seconds,@isMobile,@isAndroid,@isIOS,@agent,@ip,@ip2,@host
+INSERT INTO CI_MIDDLEWAY..logrequest (uri, [file], request, post, [started],ended,seconds,isMobile,isAndroid,isIOS, agent, ip, ip2, host, http_origin, http_referer)
+SELECT @uri, @file, @request, @post, @started, @ended,@seconds,@isMobile,@isAndroid,@isIOS,@agent,@ip,@ip2,@host, @HTTP_ORIGIN, @HTTP_REFERER
