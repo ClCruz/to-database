c44f26a95896faa21f690dae5c7762c1993dc199
Bruno Luigi Coccaro <blcoccaro@gmail.com>
2018-12-21 18:14:14 -0200

    event add - partial

---
+++ sps/pr_to_admin_user_base_select.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE dbo.pr_to_admin_user_base_select (@id_user UNIQUEIDENTIFIER)
+
+AS
+
+SELECT
+b.id_base
+,b.ds_nome_teatro
+,b.ds_nome_base_sql
+FROM CI_MIDDLEWAY..mw_base b
+INNER JOIN CI_MIDDLEWAY..to_admin_user_base taub ON b.id_base=taub.id_base
+WHERE id_to_admin_user=@id_user
