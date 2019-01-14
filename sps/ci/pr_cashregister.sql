7f5c7c0e6ffc09fcb5055934ffd63088b6dc2053
blcoccaro <blcoccaro@gmail.com>
2019-01-14 11:15:14 -0200

    drone

--- sps/ci/pr_cashregister.sql
+++ sps/ci/pr_cashregister.sql
@@ -1,17 +1,21 @@
-
-CREATE PROCEDURE pr_cashregister (@id UNIQUEIDENTIFIER)
+ALTER PROCEDURE pr_cashregister (@id UNIQUEIDENTIFIER)
  AS
 
 SET NOCOUNT ON;
 
 DECLARE @codCaixa INT, @codUsuario INT
 
+DECLARE @id_base INT
 
-SELECT
+SELECT @id_base=id_base FROM CI_MIDDLEWAY..mw_base where ds_nome_base_sql=DB_NAME()
+
+
+SELECT TOP 1
     @codCaixa=codCaixa
     ,@codUsuario=codUsuario
 FROM CI_MIDDLEWAY..ticketoffice_user_base
-WHERE id_ticketoffice_user=@id;
+WHERE id_ticketoffice_user=@id AND id_base=@id_base
+ORDER BY codCaixa DESC;
 
 IF @codCaixa IS NULL
 BEGIN
@@ -23,6 +27,5 @@ BEGIN
     FROM CI_MIDDLEWAY..ticketoffice_user_base
     WHERE id_ticketoffice_user=@id;
 END
-
 EXEC SP_MOV_UPD001 @codCaixa, @codUsuario
 
