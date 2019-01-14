eded503aeafbdbee917979a50ff48711c17bf49e
blcoccaro <blcoccaro@gmail.com>
2019-01-14 10:29:06 -0200

    drone

--- sps/ci/pr_cashregister.sql
+++ sps/ci/pr_cashregister.sql
@@ -1,12 +1,4 @@
--- SELECT
---     codCaixa
---     ,codUsuario
--- FROM CI_MIDDLEWAY..ticketoffice_user_base
--- WHERE id_ticketoffice_user='8CC26A74-7E65-411E-B854-F7B281A46E01';
 
-
--- pr_cashregister '8CC26A74-7E65-411E-B854-F7B281A46E01'
-GO
 CREATE PROCEDURE pr_cashregister (@id UNIQUEIDENTIFIER)
  AS
 
