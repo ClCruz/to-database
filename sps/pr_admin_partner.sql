9a68e3d0274d07b66c7f9e34288fee203668888d
blcoccaro <blcoccaro@gmail.com>
2019-01-16 18:42:27 -0200

    partner - partial

---
+++ sps/pr_admin_partner.sql
@@ -0,0 +1,27 @@
+ALTER PROCEDURE dbo.pr_admin_partner (@loggedId UNIQUEIDENTIFIER, @search VARCHAR(100) = NULL, @currentPage INT = 1, @perPage INT = 10)
+
+AS
+
+SET NOCOUNT ON;
+
+SELECT
+    p.id
+    ,p.active
+    ,CONVERT(VARCHAR(10),p.created,103) created
+    ,(CASE WHEN p.dateEnd IS NULL THEN '' ELSE CONVERT(VARCHAR(10),p.dateEnd,103) END) dateEnd
+    ,CONVERT(VARCHAR(10),p.dateStart,103) dateStart
+    ,p.domain
+    ,p.[key]
+    ,p.key_test
+    ,p.name
+    ,COUNT(*) OVER() totalCount
+    ,@currentPage currentPage
+FROM CI_MIDDLEWAY..[partner] p
+WHERE 
+((@search IS NULL OR p.name LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CS_AS)
+OR (@search IS NULL OR p.domain LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CS_AS)
+OR (@search IS NULL OR p.[key] LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CS_AS)
+OR (@search IS NULL OR p.[key_test] LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CS_AS))
+ORDER by p.name
+ OFFSET (@currentPage-1)*@perPage ROWS
+   FETCH NEXT @perPage ROWS ONLY;
