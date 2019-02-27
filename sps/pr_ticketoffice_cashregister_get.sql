3bae8fb8b1a312fc6dd4b9b9bd2dba731950b9c1
blcoccaro <blcoccaro@gmail.com>
2019-02-27 14:37:43 -0300

    print moviment cashregister

---
+++ sps/pr_ticketoffice_cashregister_get.sql
@@ -0,0 +1,19 @@
+CREATE PROCEDURE dbo.pr_ticketoffice_cashregister_get (@id UNIQUEIDENTIFIER)
+
+AS
+
+-- DECLARE @id UNIQUEIDENTIFIER = '0a6ef827-39ae-4cbf-b2c0-726eb460b6f3'
+
+SET NOCOUNT ON;
+
+SELECT tcrm.id
+,'R$ '+(CONVERT(VARCHAR(100),CONVERT(FLOAT,tcrm.amount)/100)) amount
+,tcrm.[type]
+,tcrm.id_ticketoffice_user
+,tcrm.justification
+,toau.[login]
+,toau.name
+,CONVERT(VARCHAR(10),tcrm.created,103) + ' ' + CONVERT(VARCHAR(8),tcrm.created,114) created
+FROM CI_MIDDLEWAY..ticketoffice_cashregister_moviment tcrm
+INNER JOIN CI_MIDDLEWAY..to_admin_user toau ON tcrm.id_ticketoffice_user=toau.id
+WHERE tcrm.id=@id
