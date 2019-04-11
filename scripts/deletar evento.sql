4d6608987147c9588ba8a28bd7a1224daf2117f4
blcoccaro <blcoccaro@gmail.com>
2019-04-11 15:02:42 -0300

    minor improvements

--- scripts/deletar evento.sql
+++ scripts/deletar evento.sql
@@ -1,11 +1,14 @@
-begin tran
+-- begin tran
 -- commit
 -- rollback
+declare @id_evento INT
+        ,@codPeca INT 
 
-delete from CI_MIDDLEWAY..mw_apresentacao_bilhete where id_apresentacao in (select id_apresentacao from CI_MIDDLEWAY..mw_apresentacao where id_evento=33009)
-delete from CI_MIDDLEWAY..mw_apresentacao where id_evento=33009
-delete from CI_MIDDLEWAY..mw_evento where id_evento=33009
-delete from tabPatrocinio where CodApresentacao in (select CodApresentacao from tabApresentacao where CodPeca=31)
-delete from tabValBilhete where CodPeca=31
-delete from tabApresentacao where CodPeca=31
-delete from tabPeca where CodPeca=31
+        
+delete from CI_MIDDLEWAY..mw_apresentacao_bilhete where id_apresentacao in (select id_apresentacao from CI_MIDDLEWAY..mw_apresentacao where id_evento=@id_evento)
+delete from CI_MIDDLEWAY..mw_apresentacao where id_evento=@id_evento
+delete from CI_MIDDLEWAY..mw_evento where id_evento=@id_evento
+delete from tabPatrocinio where CodApresentacao in (select CodApresentacao from tabApresentacao where CodPeca=@codPeca)
+delete from tabValBilhete where CodPeca=@codPeca
+delete from tabApresentacao where CodPeca=@codPeca
+delete from tabPeca where CodPeca=@codPeca
--- sps/pr_admin_event.sql
+++ sps/pr_admin_event.sql
@@ -3,6 +3,26 @@ ALTER PROCEDURE dbo.pr_admin_event (@id_user UNIQUEIDENTIFIER, @search VARCHAR(1
 
 AS
 
+-- DECLARE @id_user UNIQUEIDENTIFIER='F2177E5E-F727-4906-948D-4EEA9B9BBD0E', @search VARCHAR(100) = NULL, @currentPage INT = 1, @perPage INT = 10
+
+--SET @search = '@open'
+-- SET @search = '@web'
+
+DECLARE @onlywithshow BIT = NULL
+DECLARE @internet BIT = NULL
+
+IF @search = '@open'
+BEGIN
+    SET @search = ''
+    SET @onlywithshow = 1
+END
+
+IF @search = '@web'
+BEGIN
+    SET @search = ''
+    SET @internet = 1
+END
+
 SET NOCOUNT ON;
 
 SELECT
@@ -15,11 +35,14 @@ SELECT
     ,eei.showInBanner
     ,eei.cardimage
     ,eei.uri
+    ,eei.showonline
+    ,(CASE WHEN EXISTS(SELECT 1 FROM CI_MIDDLEWAY..mw_apresentacao sub WHERE sub.id_evento=e.id_evento AND sub.dt_apresentacao>=GETDATE()) THEN 1 ELSE 0 END) hasshowyet
     ,le.ds_local_evento
     ,g.name genreName
     ,m.ds_municipio
     ,es.ds_estado
     ,es.sg_estado
+    ,e.id_base
     ,CONVERT(VARCHAR(10),eei.created,103) + ' ' + CONVERT(VARCHAR(8),eei.created,114) [created]
     ,COUNT(*) OVER() totalCount
     ,@currentPage currentPage
@@ -30,8 +53,10 @@ INNER JOIN CI_MIDDLEWAY..to_admin_user_base taub ON e.id_base=taub.id_base AND t
 LEFT JOIN CI_MIDDLEWAY..mw_municipio m ON m.id_municipio=le.id_municipio
 LEFT JOIN CI_MIDDLEWAY..mw_estado es ON m.id_estado=es.id_estado
 LEFT JOIN CI_MIDDLEWAY..genre g ON eei.id_genre=g.id
-WHERE ((@search IS NULL OR e.ds_evento LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CI_AS)
-OR (@search IS NULL OR le.ds_local_evento LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CI_AS))
-ORDER by e.ds_evento
+WHERE (((@search IS NULL OR e.ds_evento LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CI_AS)
+OR (@search IS NULL OR le.ds_local_evento LIKE '%'+@search+'%' COLLATE SQL_Latin1_General_Cp1251_CI_AS)))
+AND (@onlywithshow IS NULL OR (CASE WHEN EXISTS(SELECT 1 FROM CI_MIDDLEWAY..mw_apresentacao sub WHERE sub.id_evento=e.id_evento AND sub.dt_apresentacao>=GETDATE()) THEN 1 ELSE 0 END)=@onlywithshow)
+AND (@internet IS NULL OR eei.showonline=@internet)
+ORDER by (CASE WHEN EXISTS(SELECT 1 FROM CI_MIDDLEWAY..mw_apresentacao sub WHERE sub.id_evento=e.id_evento AND sub.dt_apresentacao>=GETDATE()) THEN 0 ELSE 1 END), e.ds_evento
  OFFSET (@currentPage-1)*@perPage ROWS
     FETCH NEXT @perPage ROWS ONLY;
