cf919c2e47f905e1a251f225b9dd6785a2305583
blcoccaro <blcoccaro@gmail.com>
2019-05-08 14:59:16 -0300

    proximo eventos

--- sps/pr_geteventsforcards.sql
+++ sps/pr_geteventsforcards.sql
@@ -1,4 +1,5 @@
 -- EXEC pr_geteventsforcards @api='live_578abaf329f84119bb7c1e55dfdc7e0f4f20e693cd2c4bc7a5bc0a0965fae322'
+-- exec sp_executesql N'EXEC pr_geteventsforcards @P1, @P2, @P3, @P4',N'@P1 nvarchar(4000),@P2 nvarchar(4000),@P3 nvarchar(4000),@P4 nvarchar(4000)',N'',N'',N'live_279e0f576f1547faa1ac5600d7802778f3754aa3f6d44f9aabbdbb709b2ed442',N'created'
 
 ALTER PROCEDURE dbo.pr_geteventsforcards (@city VARCHAR(100) = NULL,@state VARCHAR(100) = NULL, @api VARCHAR(100) = NULL, @date DATETIME =NULL, @filter VARCHAR(100) = NULL)
 
@@ -7,7 +8,8 @@ AS
 -- update CI_MIDDLEWAY..mw_evento_extrainfo set minAmount=1000 where id_evento=32947
 --  update CI_MIDDLEWAY..mw_evento_extrainfo set minAmount=1000, maxAmount=3000 where id_evento=33016
 
---DECLARE @city VARCHAR(100) = NULL,@state VARCHAR(100) = NULL, @date DATETIME ='', @api VARCHAR(100) = 'live_c02e42c0d2524044b12ec3c3ee77e5a7e6d56d5ccc274dd3ba4690137492741e' ,@filter VARCHAR(1000) = 'created'
+-- DECLARE @city VARCHAR(100) = NULL,@state VARCHAR(100) = NULL, @date DATETIME ='', @api VARCHAR(100) = 'live_665495f829b74054b8bbfa1b482b40587c7a1781718c44a591b254d862b2b02d' ,@filter VARCHAR(1000) = 'created'
+
 IF @date = '1900-01-01 00:00:00.000'
     SET @date = NULL
 
@@ -82,10 +84,7 @@ h.id_evento
 
 ORDER BY 
 (CASE WHEN @filter = 'created' THEN eei.created END) DESC,
-
-
-
-
+(CASE WHEN @filter = 'next' THEN min(ap.dt_apresentacao) END) DESC,
 (CASE WHEN h.ds_municipio = @city COLLATE Latin1_general_CI_AI THEN 1
                 WHEN h.ds_municipio != @city COLLATE Latin1_general_CI_AI
                      AND h.sg_estado = @state COLLATE Latin1_general_CI_AI THEN 2
