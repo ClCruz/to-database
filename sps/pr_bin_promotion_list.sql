7d60cdbb3f8bb648e496f02c8ef8f21cae277997
blcoccaro <blcoccaro@gmail.com>
2019-08-14 10:23:02 -0300

    pequenas correções

---
+++ sps/pr_bin_promotion_list.sql
@@ -0,0 +1,109 @@
+-- exec sp_executesql N'EXEC pr_bin_promotion_list @P1, @P2',N'@P1 nvarchar(4000),@P2 nvarchar(4000)',N'2019-07-01 00:00:00',N'2019-07-31 23:59:59'
+-- exec sp_executesql N'EXEC pr_bin_promotion_list @P1, @P2',N'@P1 nvarchar(4000),@P2 nvarchar(4000)',N'2019-07-01 00:00:00',N'2019-07-31 23:59:59'
+-- exec sp_executesql N'EXEC pr_bin_promotion_list @P1, @P2',N'@P1 nvarchar(4000),@P2 nvarchar(4000)',N'2019-07-01 00:00:00',N'2019-07-31 23:59:59'
+
+ALTER PROCEDURE dbo.pr_bin_promotion_list (@start DATETIME, @end DATETIME)
+
+AS
+
+SET NOCOUNT ON;
+
+-- DECLARE @start DATETIME = '2019-07-01 00:00:00', @end DATETIME = '2019-07-31 23:59:59'
+
+IF OBJECT_ID('tempdb.dbo.#result', 'U') IS NOT NULL
+    DROP TABLE #result; 
+
+IF OBJECT_ID('tempdb.dbo.#bases', 'U') IS NOT NULL
+    DROP TABLE #bases; 
+
+CREATE TABLE #result (sellid VARCHAR(1000), created_at VARCHAR(10), dt_pedido_venda DATETIME, buyer VARCHAR(1000), buyer_document VARCHAR(100), bin VARCHAR(100), sellfrom VARCHAR(100), vl_total_pedido_venda DECIMAL(12,2), sellamount VARCHAR(1000), sponsor VARCHAR(1000))
+
+
+-- Data Venda, Nome Comprador, CPF comprador, Bin do cartão, Web ou Bilheteria, Valor da Venda.
+
+INSERT INTO #result (sellid,created_at,dt_pedido_venda,buyer,buyer_document,bin,sellfrom,vl_total_pedido_venda,sellamount,sponsor)
+SELECT DISTINCT
+ipv.CodVenda
+,CONVERT(VARCHAR(10),pv.dt_pedido_venda,103) created_at
+,pv.dt_pedido_venda
+,(c.ds_nome + ' ' + c.ds_sobrenome) buyer
+,c.cd_cpf buyer_document
+,pv.cd_bin_cartao bin
+,'web' [sellfrom]
+,pv.vl_total_pedido_venda
+,FORMAT(CONVERT(DECIMAL(12,2),pv.vl_total_pedido_venda), 'N', 'pt-br') sellamount
+,p.ds_NomPatrocinador sponsor
+FROM CI_MIDDLEWAY..mw_pedido_venda pv
+INNER JOIN CI_MIDDLEWAY..mw_item_pedido_venda ipv ON pv.id_pedido_venda=ipv.id_pedido_venda
+INNER JOIN CI_MIDDLEWAY..mw_apresentacao ap ON ipv.id_apresentacao=ap.id_apresentacao
+INNER JOIN CI_MIDDLEWAY..mw_cliente c ON pv.id_cliente=c.id_cliente
+INNER JOIN CI_MIDDLEWAY..mw_controle_evento ce ON ap.id_evento=ce.id_evento
+INNER JOIN CI_MIDDLEWAY..mw_promocao_controle pc ON ce.id_promocao_controle=pc.id_promocao_controle AND pv.dt_pedido_venda BETWEEN pc.dt_inicio_promocao AND pc.dt_fim_promocao
+LEFT JOIN CI_MIDDLEWAY..mw_cartao_patrocinado cp ON pv.cd_bin_cartao=cp.cd_bin AND cp.id_patrocinador=pc.id_patrocinador
+LEFT JOIN CI_MIDDLEWAY..mw_patrocinador p ON pc.id_patrocinador=p.id_Patrocinador
+LEFT JOIN CI_MIDDLEWAY..mw_bandeira_cartao bc ON cp.id_bandeira_cartao=bc.id_bandeira_cartao
+WHERE pv.dt_pedido_venda BETWEEN @start AND @end AND pv.cd_bin_cartao IS NOT NULL
+AND pv.in_situacao='F'
+
+
+SELECT DISTINCT tosh.id_base,0 done
+INTO #bases
+FROM CI_MIDDLEWAY..ticketoffice_shoppingcart_hist tosh
+INNER JOIN CI_MIDDLEWAY..ticketoffice_pinpad topp ON tosh.id_ticketoffice_user=topp.id_ticketoffice_user
+WHERE tosh.sell_date BETWEEN @start AND @end
+
+-- return;
+WHILE (EXISTS (SELECT 1 FROM #bases WHERE done=0 ))
+BEGIN
+    DECLARE @currentBase INT = 0
+            ,@db_name VARCHAR(1000)
+            ,@toExec NVARCHAR(MAX)
+
+    SELECT TOP 1 @currentBase=id_base FROM #bases WHERE done=0 ORDER BY id_base
+    SELECT TOP 1 @db_name=b.ds_nome_base_sql FROM CI_MIDDLEWAY..mw_base b WHERE b.id_base=@currentBase;
+
+    SET @toExec=''
+    SET @toExec = @toExec + 'INSERT INTO #result (sellid,created_at,dt_pedido_venda,buyer,buyer_document,bin,sellfrom,vl_total_pedido_venda,sellamount,sponsor) '
+    SET @toExec = @toExec + ' SELECT DISTINCT  '
+    SET @toExec = @toExec + ' tosh.codVenda '
+    SET @toExec = @toExec + ' , CONVERT(VARCHAR(10),tosh.sell_date,103) created_at '
+    SET @toExec = @toExec + ' , tosh.sell_date '
+    SET @toExec = @toExec + ' , c.Nome buyer '
+    SET @toExec = @toExec + ' , c.CPF buyer_document '
+    SET @toExec = @toExec + ' , ls.BINCartao bin '
+    SET @toExec = @toExec + ' , ''bilheteria'' sellfrom '
+    SET @toExec = @toExec + ' , CONVERT(DECIMAL(12,2),tosh.amount_topay)/CONVERT(DECIMAL(12,2),100) vl_total_pedido_venda '
+    SET @toExec = @toExec + ' , FORMAT(CONVERT(DECIMAL(12,2),tosh.amount_topay)/CONVERT(DECIMAL(12,2),100), ''N'', ''pt-br'') sellamount '
+    SET @toExec = @toExec + ' , p.ds_NomPatrocinador sponsor '
+    SET @toExec = @toExec + ' FROM ['+@db_name+'].dbo.tabLugSala ls  '
+    SET @toExec = @toExec + ' INNER JOIN CI_MIDDLEWAY..ticketoffice_shoppingcart_hist tosh ON tosh.indice=ls.Indice AND tosh.codVenda=ls.CodVenda COLLATE SQL_Latin1_General_CP1_CI_AS'
+    SET @toExec = @toExec + ' INNER JOIN ['+@db_name+'].dbo.tabHisCliente hc ON hc.Indice=tosh.indice AND ls.CodApresentacao=hc.CodApresentacao '
+    SET @toExec = @toExec + ' INNER JOIN ['+@db_name+'].dbo.tabCliente c ON hc.Codigo=c.Codigo '
+    SET @toExec = @toExec + ' INNER JOIN CI_MIDDLEWAY..mw_evento e ON e.CodPeca=tosh.id_event AND e.id_base=tosh.id_base '
+    SET @toExec = @toExec + ' INNER JOIN CI_MIDDLEWAY..mw_controle_evento ce ON e.id_evento=ce.id_evento '
+    SET @toExec = @toExec + ' INNER JOIN CI_MIDDLEWAY..mw_promocao_controle pc ON ce.id_promocao_controle=pc.id_promocao_controle AND tosh.created BETWEEN pc.dt_inicio_promocao AND pc.dt_fim_promocao '
+    SET @toExec = @toExec + ' INNER JOIN CI_MIDDLEWAY..mw_cartao_patrocinado cp ON cp.id_patrocinador=pc.id_patrocinador AND ls.BINCartao=cp.cd_bin  COLLATE SQL_Latin1_General_CP1_CI_AS '
+    SET @toExec = @toExec + ' INNER JOIN CI_MIDDLEWAY..mw_patrocinador p ON pc.id_patrocinador=p.id_Patrocinador '
+    SET @toExec = @toExec + ' WHERE tosh.created BETWEEN @start AND @end AND ls.BINCartao IS NOT NULL '
+    -- SET @toExec = @toExec + ' ORDER BY tosh.sell_date desc '
+
+-- print @toExec
+    EXEC sp_executesql @toExec, N'@start DATETIME, @end DATETIME',@start,@end
+	
+    UPDATE #bases SET done=1 WHERE id_base=@currentBase;
+END
+
+SELECT DISTINCT
+r.buyer
+,r.buyer_document
+,r.bin
+,r.sellid
+,r.created_at
+,r.sellfrom
+,r.sellamount
+,r.sponsor
+,r.dt_pedido_venda
+,FORMAT(CONVERT(DECIMAL(12,2),(SELECT SUM(sub.vl_total_pedido_venda) FROM #result sub)), 'N', 'pt-br') selltotal
+FROM #result r
+ORDER BY r.dt_pedido_venda DESC
+
--- sps/pr_pinpad_update.sql
+++ sps/pr_pinpad_update.sql
@@ -1,13 +1,26 @@
--- exec sp_executesql N'EXEC pr_pinpad_update @P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8',N'@P1 nvarchar(4000),@P2 nvarchar(4000),@P3 nvarchar(4000),@P4 nvarchar(4000),@P5 nvarchar(4000),@P6 nvarchar(4000),@P7 nvarchar(4000),@P8 nvarchar(4000)',N'B23C243A49684A9DBD6730AAA5958563',N'0000',N'6191130',N'1',N'0',N'0',N'1',N'0'
---exec sp_executesql N'EXEC pr_pinpad_update @P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8',N'@P1 nvarchar(4000),@P2 nvarchar(4000),@P3 nvarchar(4000),@P4 nvarchar(4000),@P5 nvarchar(4000),@P6 nvarchar(4000),@P7 nvarchar(4000),@P8 nvarchar(4000)',N'9BE771FE88C641E5AB1EB88F4E782EB9',N'0000',N'6191154',N'1',N'0',N'0',N'1',N'0'
--- select * from ticketoffice_gateway_result
+--exec sp_executesql N'EXEC pr_pinpad_update @P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8,@P9',N'@P1 nvarchar(4000),@P2 nvarchar(4000),@P3 nvarchar(4000),@P4 nvarchar(4000),@P5 nvarchar(4000),@P6 nvarchar(4000),@P7 nvarchar(4000),@P8 nvarchar(4000),@P9 nvarchar(4000)',N'33067F4CC9EE44039009A04D65C690F7',N'0000',N'6785166',N'1',N'0',N'0',N'1',N'0',N'IMIOIGDHAI'
 
 -- select top 100 * from CI_MIDDLEWAY..ticketoffice_pinpad order by created desc
 ALTER PROCEDURE dbo.pr_pinpad_update (@key VARCHAR(50),@pinpad_acquirerResponseCode VARCHAR(100),@pinpad_transactionId VARCHAR(100)
                                     ,@pinpad_executed BIT,@pinpad_error BIT,@pinpad_cancel BIT
-                                    ,@pinpad_ok BIT,@pinpad_fail BIT,@codVenda VARCHAR(10))
+                                    ,@pinpad_ok BIT,@pinpad_fail BIT,@codVenda VARCHAR(10),@bin VARCHAR(10) = NULL)
 
 AS
+
+-- DECLARE @key VARCHAR(50) = '33067F4CC9EE44039009A04D65C690F7'
+--         ,@pinpad_acquirerResponseCode VARCHAR(100) = '0000'
+--         ,@pinpad_transactionId VARCHAR(100) = '6785166'
+--         ,@pinpad_executed BIT = 1
+--         ,@pinpad_error BIT = 0
+--         ,@pinpad_cancel BIT = 0
+--         ,@pinpad_ok BIT = 1
+--         ,@pinpad_fail BIT = 0
+--         ,@codVenda VARCHAR(10) = 'IMIOIGDHAI'
+--         ,@bin VARCHAR(10) = NULL
+
+
+-- select * from ticketoffice_pinpad where [key]=@key
+SET NOCOUNT ON;
 
 UPDATE CI_MIDDLEWAY..ticketoffice_pinpad
 SET
@@ -19,10 +32,31 @@ pinpad_acquirerResponseCode=@pinpad_acquirerResponseCode
 ,pinpad_ok=@pinpad_ok
 ,pinpad_fail=@pinpad_fail
 ,codVenda=@codVenda
+,bin=@bin
 WHERE [key]=@key
 
 INSERT INTO CI_MIDDLEWAY..ticketoffice_gateway_result (id_ticketoffice_user, id_ticketoffice_shoppingcart, transactionKey, id_gateway)
 SELECT TOP 1 topp.id_ticketoffice_user, tosc.id, topp.pinpad_transactionId, 6
 FROM CI_MIDDLEWAY..ticketoffice_pinpad topp
 INNER JOIN CI_MIDDLEWAY..ticketoffice_shoppingcart_hist tosc ON topp.codVenda=tosc.codVenda
+LEFT JOIN CI_MIDDLEWAY..ticketoffice_gateway_result togr ON topp.pinpad_transactionId=togr.transactionKey AND togr.id_gateway=6
+WHERE [key]=@key AND togr.id IS NULL
+
+
+DECLARE @id_base INT
+        ,@db_name VARCHAR(1000)
+        ,@toExec NVARCHAR(MAX) = ''
+        ,@StaCadeira VARCHAR(1) = 'V'
+
+SELECT @id_base = tosc.id_base,@db_name=b.ds_nome_base_sql
+FROM CI_MIDDLEWAY..ticketoffice_pinpad topp
+INNER JOIN CI_MIDDLEWAY..ticketoffice_shoppingcart_hist tosc ON topp.codVenda=tosc.codVenda
+INNER JOIN CI_MIDDLEWAY..mw_base b ON tosc.id_base=b.id_base
 WHERE [key]=@key
+
+SET @toExec=''
+SET @toExec = @toExec+' UPDATE ['+@db_name+'].dbo.tabLugSala'
+SET @toExec = @toExec+' SET BINCartao=@bin'
+SET @toExec = @toExec+' WHERE CodVenda=@codVenda AND StaCadeira=@StaCadeira'
+EXEC sp_executesql @toExec, N'@bin VARCHAR(100), @codVenda VARCHAR(100), @StaCadeira VARCHAR(1)',@bin,@codVenda,@StaCadeira
+
