2ead31668ce7cd5c5a60169c041200ec4353b75b
blcoccaro <blcoccaro@gmail.com>
2019-08-22 20:28:53 +0200

    verificação de uma SP

--- sps/ci/pr_typepaymenttype_save.sql
+++ sps/ci/pr_typepaymenttype_save.sql
@@ -1,19 +1,7 @@
-CREATE PROCEDURE dbo.pr_paymenttype_save (@CodForPagto INT
-
-  ,@CodBanco
-  ,@CodForPagto
-  ,@CodTipForPagto
-  ,@ForPagto
-  ,@PcTxAdm
-  ,@PrzRepasseDias
-  ,@showorder
-  ,@StaDebBordLiq
-  ,@StaForPagto
-  ,@StaPagarMe
-  ,@StaTaxaCartoes
-  ,@TipCaixa
-
-
+CREATE PROCEDURE dbo.pr_typepaymenttype_save (@CodTipForPagto INT
+,@ClassifPagtoSAP VARCHAR(10)
+,@StaTipForPagto VARCHAR(1)
+,@TipForPagto VARCHAR(1000)
 )
 
 AS
@@ -35,27 +23,6 @@ BEGIN
         ,[StaTipForPagto] = @StaTipForPagto
         ,[ClassifPagtoSAP] = @ClassifPagtoSAP
     WHERE [CodTipForPagto] = @CodTipForPagto
-
-    USE [ci_localhost]
-GO
-
-UPDATE [dbo].[tabForPagamento]
-   SET [CodForPagto] = <CodForPagto, tinyint,>
-      ,[CodTipForPagto] = <CodTipForPagto, tinyint,>
-      ,[ForPagto] = <ForPagto, varchar(30),>
-      ,[CodBanco] = <CodBanco, char(3),>
-      ,[StaForPagto] = <StaForPagto, char(1),>
-      ,[TipCaixa] = <TipCaixa, char(1),>
-      ,[PcTxAdm] = <PcTxAdm, money,>
-      ,[PrzRepasseDias] = <PrzRepasseDias, int,>
-      ,[StaDebBordLiq] = <StaDebBordLiq, char(1),>
-      ,[StaTaxaCartoes] = <StaTaxaCartoes, char(1),>
-      ,[StaPagarMe] = <StaPagarMe, char(1),>
-      ,[showorder] = <showorder, int,>
- WHERE <Search Conditions,,>
-GO
-
-
 END
 ELSE
 BEGIN
@@ -66,39 +33,6 @@ BEGIN
         SET @idDB = 1
     END
     SET @CodTipForPagto =@idDB
-
-
-USE [ci_localhost]
-GO
-
-INSERT INTO [dbo].[tabForPagamento]
-           ([CodForPagto]
-           ,[CodTipForPagto]
-           ,[ForPagto]
-           ,[CodBanco]
-           ,[StaForPagto]
-           ,[TipCaixa]
-           ,[PcTxAdm]
-           ,[PrzRepasseDias]
-           ,[StaDebBordLiq]
-           ,[StaTaxaCartoes]
-           ,[StaPagarMe]
-           ,[showorder])
-     VALUES
-           (<CodForPagto, tinyint,>
-           ,<CodTipForPagto, tinyint,>
-           ,<ForPagto, varchar(30),>
-           ,<CodBanco, char(3),>
-           ,<StaForPagto, char(1),>
-           ,<TipCaixa, char(1),>
-           ,<PcTxAdm, money,>
-           ,<PrzRepasseDias, int,>
-           ,<StaDebBordLiq, char(1),>
-           ,<StaTaxaCartoes, char(1),>
-           ,<StaPagarMe, char(1),>
-           ,<showorder, int,>)
-GO
-
 
 
     INSERT INTO [dbo].[tabTipForPagamento]
