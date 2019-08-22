a5815bd990639051adb08fa369325aa1c7865b5b
blcoccaro <blcoccaro@gmail.com>
2019-08-22 20:09:13 +0200

    verificação de uma SP

--- sps/ci/pr_room_sectors_add.sql
+++ sps/ci/pr_room_sectors_add.sql
@@ -1,7 +1,3 @@
--- exec sp_executesql N'EXEC pr_room_sectors_add @P1,@P2,@P3,@P4',N'@P1 nvarchar(4000),@P2 nvarchar(4000),@P3 nvarchar(4000),@P4 nvarchar(4000)',N'4',N'teste',N'3.21',N'#134f5c'
-delete from tabSetor where CodSala=4
-delete from tabsala where CodSala=4
-
 CREATE PROCEDURE dbo.pr_room_sectors_add (@codSala INT
                                             ,@name VARCHAR(1000)
                                             ,@value FLOAT
